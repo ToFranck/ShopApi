1a417aab6d41532845f7db4b76e75d664746bb43
ToFranck <73122097+ToFranck@users.noreply.github.com>
2023-07-01 18:11:06 +0200

    ouais

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
+// https://fakestoreapi.com/docs
+
 function App() {
+  
+  const [products, setProducts] = useState([])
+  const [users , setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+  
+    .then(res => res.json())
+    .then(json => setProducts(json))
+  }, [])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    
+    .then(res => res.json())
+    .then(json => setUsers(json))
+  }, [])
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Products</h1>
+        <ul>
+          {products.map(product => (
+            <li key={product.id}>
+              <h2>{product.title}</h2>
+              {/* <p>{product.description}</p> */}
+              {/* <img src={product.image} alt={product.title} style={
+                {width: '200px'}
+              }/> */}
+            </li>
+          ))}
+        </ul>
+
+        <h1>Users</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </li>
+          ))}
+        </ul>
+        
+        
+
+
+
     </div>
   );
 }
